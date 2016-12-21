e9fed026d7325d90e81b04d3380f7799853a149b
Slavi007 <lordi_slavi@abv.bg>
2016-12-21 09:38:12 +0200

    Uploading Images

--- PicBook/Controllers/HomeController.cs
+++ PicBook/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace PicBook.Controllers
 {
@@ -12,19 +13,10 @@ namespace PicBook.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult GetUsers()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var users = Membership.GetAllUsers();
+            return View(users);
         }
     }
 }
